74923493fd0add9ea7d5b04a68669f7dbb8d8c5f
Aleksandra-Aleksandrovna-K <sashasuper@list.ru>
2019-11-25 16:08:40 +0300

    v1

---
+++ src/task2.cpp
@@ -0,0 +1,12 @@
+bool checkPrime(unsigned int value)
+{
+	
+}
+unsigned long long nPrime(unsigned n)
+{
+	
+}
+unsigned long long nextPrime(unsigned long long value)
+{
+	
+}
---
+++ src/task3.cpp
@@ -0,0 +1,5 @@
+unsigned long long sumPrime(unsigned int hbound)
+{
+	
+}
+
