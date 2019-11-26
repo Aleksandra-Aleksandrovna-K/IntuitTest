56fd726f698595d470355efa87ee4d8c776abf4c
Aleksandra-Aleksandrovna-K <sashasuper@list.ru>
2019-11-26 17:21:03 +0300

    v2

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <math.h>
+#include "task2.h"
 using namespace std;
 
-int main() 
-{ 
-    cout << "Hello, world!" << endl;
-    return 0; 
+int main()
+{
+    unsigned int a;
+	unsigned b;
+	unsigned long long c;
+    printf("Enter a number: ");//выводим на экран сообщение
+	scanf("%u", &a);//помещаем введённое число в a
+	if (checkPrime(a))
+	{
+		printf("This is a prime number\n");
+	}
+	else
+	{
+		printf("This is not a prime number\n");
+	}
+		
+	printf("Enter the number of prime numbers: ");//выводим на экран сообщение
+	scanf("%u", &b);//помещаем введённое число в b
+	printf("Your prime number is: %llu \n",nPrime(b));
+	
+	printf("Enter a number to find the nearest next prime number: ");//выводим на экран сообщение
+	scanf("%llu", &c);//помещаем введённое число в c
+	printf("Your nearest next prime number is: %llu \n",nextPrime(c));
+	
 }
+    
--- src/task2.cpp
+++ src/task2.cpp
@@ -1,12 +1,55 @@
 bool checkPrime(unsigned int value)
 {
-	
+    int i;
+    if (abs(value)<=3)
+    {
+        return true;
+    }
+    for (i = 2; i <= (sqrt(abs(value))); i++)//проверяем остаток до корня из числа
+    {
+        if (value%i==0)
+        {
+            return false;
+            break;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
 unsigned long long nPrime(unsigned n)
 {
-	
+	int a=1;//счётчик найденных простых чисел
+	int b=0;//переменная для запоминания текущего простого числа
+	bool j;//переменная для запоминания результата проверки числа
+	unsigned int i;//переменная для перебора чисел
+	for (i = 1; a <=n; i++)
+	{
+		j=checkPrime(i);
+		if (j)
+		{
+			b=i;
+			a++;
+		}
+	}
+	return b;
 }
 unsigned long long nextPrime(unsigned long long value)
 {
-	
+	int a=0;//переменная для результата
+	bool j;//переменная для запоминания результата проверки числа
+	int b=0;//переменная для запоминания текущего простого числа
+	int i;//переменная цикла
+	for (i = value+1; a <= 1; i++)
+	{
+		j=checkPrime(i);
+		if (j)
+		{
+			b=i;
+			a=1;
+			break;
+		}
+	}
+	return b;
 }
